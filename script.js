67ebce0a509e80c9126a3f285b9bece3bf24822b
vishva-kalhara <vishvakalhara@gmail.com>
2023-08-08 13:12:50 +0530

    Final Commit ✅

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const content = document.getElementById("content");
 
 const marksArr = [
 	72.52, 78.56, 75, 85, 99, 85, 72.52, 78.56, 75, 85, 99, 85, 72.52, 78.56,
-
 ]; // MARKS
 const studentArr = [1, 2, 3, 4, 5, 6, 7, 8];
 const subjectArr = [1, 2, 3];
 const termArr = [1, 2];
+const db = "exam_marks_db";
+const table = "student_exam_mark_record";
 // let mark;
 
 let index = 0;
@@ -29,14 +30,16 @@ let q = "";
 const generateQuery = function (result, index) {
 	// let res = algorithm();
 	let newQuesry =
-		"INSERT INTO `exam_marks_db`.`student_exam_mark_record` (`student_index_no`,`subject_subject_id`,`exam_term_term_id`,`mark`) VALUES (" +
+		"INSERT INTO `" +
+		db +
+		"`.`" +
+		table +
+		"` (`student_index_no`,`subject_subject_id`,`exam_term_term_id`,`mark`) VALUES (" +
 		result +
 		");";
+		
 	q = q + " " + newQuesry;
 	if (index == marksArr.length - 1) content.innerHTML = q;
 };
 
 algorithm();
-
-const baseQ =
-	"INSERT INTO `exam_marks_db`.`student_exam_mark_record` (`student_index_no`,`subject_subject_id`,`exam_term_term_id`,`mark`) VALUES ()";
